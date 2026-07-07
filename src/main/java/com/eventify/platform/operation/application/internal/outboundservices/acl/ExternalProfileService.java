d1aaa7a5ae2a9e1ebf5fd73b688d82a65cb75d21
Deybbi Crisanto <u202120569@upc.edu.pe>
2026-07-07 00:28:31 -0500

    feat: enhance ExternalProfileService with detailed Javadoc comments for better documentation

--- src/main/java/com/eventify/platform/operation/application/internal/outboundservices/acl/ExternalProfileService.java
+++ src/main/java/com/eventify/platform/operation/application/internal/outboundservices/acl/ExternalProfileService.java
@@ -6,18 +6,31 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * Service class responsible for interacting with the ProfilesContextFacade
+ * to fetch profile-related information.
+ */
 @Service
 public class ExternalProfileService {
     private final ProfilesContextFacade profilesContextFacade;
 
+    /**
+     * Constructs an ExternalProfileService with the specified ProfilesContextFacade.
+     *
+     * @param profilesContextFacade the facade used to interact with the profiles context
+     */
     public ExternalProfileService(ProfilesContextFacade profilesContextFacade){
         this.profilesContextFacade = profilesContextFacade;
     }
 
+    /**
+     * Fetches the full name of a profile based on the given profile ID.
+     *
+     * @param profileId the ID of the profile whose full name is to be fetched
+     * @return an Optional containing the full name if found, or an empty Optional if not
+     */
     public Optional<String> fetchFullNameByProfileId(Long profileId) {
         var fullName = profilesContextFacade.fetchFullNameByProfileId(profileId);
         return fullName == null ? Optional.empty() : Optional.of(fullName);
     }
-
-
 }

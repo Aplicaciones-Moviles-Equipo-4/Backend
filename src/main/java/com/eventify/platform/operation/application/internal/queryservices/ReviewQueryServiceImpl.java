0ce73ba2ba3c3495a3f1b65abfad8f731243ea78
Deybbi Crisanto <u202120569@upc.edu.pe>
2026-07-07 00:28:48 -0500

    feat: add Javadoc comments to ReviewQueryServiceImpl for improved documentation

--- src/main/java/com/eventify/platform/operation/application/internal/queryservices/ReviewQueryServiceImpl.java
+++ src/main/java/com/eventify/platform/operation/application/internal/queryservices/ReviewQueryServiceImpl.java
@@ -12,27 +12,64 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Implementation of the ReviewQueryService interface.
+ * Provides methods to handle queries related to reviews.
+ */
 @Service
 public class ReviewQueryServiceImpl implements ReviewQueryService {
     private ReviewRepository reviewRepository;
 
+    /**
+     * Constructs a ReviewQueryServiceImpl with the specified ReviewRepository.
+     *
+     * @param reviewRepository the repository used to access review data
+     */
     public ReviewQueryServiceImpl(ReviewRepository reviewRepository){
         this.reviewRepository = reviewRepository;
     }
 
+    /**
+     * Handles the query to retrieve a review by its ID.
+     *
+     * @param query the query containing the review ID
+     * @return an Optional containing the review if found, or an empty Optional if not
+     */
     @Override
     public Optional<Review> handle(GetReviewByIdQuery query) {
         return reviewRepository.findById(query.reviewId());
     }
 
+    /**
+     * Handles the query to retrieve all reviews associated with a specific profile ID.
+     *
+     * @param query the query containing the profile ID
+     * @return a list of reviews associated with the profile ID
+     */
     @Override
-    public List<Review> handle(GetReviewByProfileIdQuery query) { return reviewRepository.findByProfileId(query.profileId());}
+    public List<Review> handle(GetReviewByProfileIdQuery query) {
+        return reviewRepository.findByProfileId(query.profileId());
+    }
 
+    /**
+     * Handles the query to retrieve all reviews.
+     *
+     * @param query the query to retrieve all reviews
+     * @return a list of all reviews
+     */
     @Override
     public List<Review> handle(GetAllReviewsQuery query) {
         return reviewRepository.findAll();
     }
 
+    /**
+     * Handles the query to check if a review exists for a specific profile ID.
+     *
+     * @param query the query containing the profile ID
+     * @return true if a review exists for the profile ID, false otherwise
+     */
     @Override
-    public boolean handle(ExistByProfileIdQuery query) { return reviewRepository.existsByProfileId(query.profileId());}
+    public boolean handle(ExistByProfileIdQuery query) {
+        return reviewRepository.existsByProfileId(query.profileId());
+    }
 }
